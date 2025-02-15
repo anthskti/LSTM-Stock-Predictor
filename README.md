b20b3f8dffd39f35edca2c930ecd2c6a52173aef
anth <phamanthony47@gmail.com>
2025-02-15 00:53:39 -0500

    Moving Averages, Creating Chart, Splitting Data

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # LSTM-Google-Model
+Machine Learning Model for Goog Stock Share. 
+Predicting the question: "Will tomorrows price be higher then than today's closing price?".
+
+Create Virtual Environment:
+python3 -m venv venv
+
+Install the Dependencies:
+pip install yfinance pandas numpy plotly scikit-learn tensor flow
+
+To run the Script:
+python3 lstm_google_stock.py
--- lstm_google_stock.py
+++ lstm_google_stock.py
@@ -1,4 +1,40 @@
 import yfinance as yf
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from sklearn.preprocessing import MinMaxScalar
+from sklearn.model_selection import train_test_split
 
-# to fetch the google stock data
-google_stock_data = yf.download('GOOG', period='2y')
+
+
+
+# to fetch the google stock data, downloading two years worth of data
+google_data = yf.download('GOOG', period='2y')
+
+# computing the moving averages of 5, 20, 50
+google_data['MA_5'] = google_data['Close'].rollowing(window=5).mean()
+google_data['MA_20'] = google_data['Close'].rollowing(window=20).mean()
+google_data['MA_50'] = google_data['Close'].rollowing(window=50).mean()
+
+# our features
+features = ['MA_5', "MA_20", "MA-50", "Clsoe"]
+
+# Scalar is used to normalize data so the features are scalled between 0 and 1 for the LSTM performance
+scalar = MinMaxScalar()
+google_data_scaled = scalar.fit_transform(google_data[features]);
+
+# prepping for output
+# Creates a Target such that, if the price is higher than today, will output 1. 
+google_data['Target'] = (google_data['Close'].shift(-1) > google_data['Close']).astype(int)
+google_data.dropna(inplace=True)
+
+# Spllitng data
+x = google_data[:-1] # drop last row since there is no "tomorrow" to compare with
+y = google_data['Target'].values[:-1]
+
+# Spliting the data for training and testing sets
+# test_size=0.2 for 20% testing and 80% training.
+# random_state=42 to ensure reproducibility, consistency in split everytime.
+# randomizes the splitting of taking the first 80%
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=42, shuffle=True)
